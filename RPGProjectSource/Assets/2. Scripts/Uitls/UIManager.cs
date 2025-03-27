3d8bba31b67a7fa7322dcb3d7b266fbb71af15a1
leewootak <leewt32414@gmail.com>
2025-03-27 20:49:46 +0900

    [feat] UIManager에 BaseUI, ShopUI 연결

--- RPGProjectSource/Assets/2. Scripts/Uitls/UIManager.cs
+++ RPGProjectSource/Assets/2. Scripts/Uitls/UIManager.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    private UIManager instance;
-    public UIManager Instance { get { return instance; } }
+    // 싱글톤 인스턴스
+    private static UIManager instance;
+    public static UIManager Instance { get { return instance; } }
 
+    // 항상 활성화 되는 UI 참조
     private BaseUI baseUI;
     public BaseUI BaseUI
     {
         get { return baseUI; }
         private set { baseUI = value; } }
 
+    // 상점 UI 참조
     private ShopUI shopUI;
     public ShopUI ShopUI
     {
@@ -22,15 +23,28 @@ public class UIManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance == null)
+        // 중복 방지
+        if (instance == null)
         {
             instance = this;
         }
-        else if (Instance != this)
+        else if (instance != this)
         {
             Destroy(gameObject);
         }
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    // 기본 UI 연결
+    public void SetBaseUI(BaseUI baseUI)
+    {
+        BaseUI = baseUI;
+    }
+
+    // 상점 UI 연결
+    public void SetShopUI(ShopUI shopUI)
+    {
+        ShopUI = shopUI;
     }
 }
