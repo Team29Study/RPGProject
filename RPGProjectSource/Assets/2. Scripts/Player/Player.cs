9de0dcd3b7195d25bc4bc46e34cf479e68625d83
park8306 <kosaf0804@gmail.com>
2025-03-27 16:26:12 +0900

    [feat] StateMachine 추상 클래스 구현

---
+++ RPGProjectSource/Assets/2. Scripts/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 플레이어의 Component들을 관리하는 클래스
+public class Player : MonoBehaviour
+{
+    [field:Header("Animations")]
+    [field: SerializeField] public PlayerAnimationData AnimationData { get; private set; } 
+
+    public Animator Animator { get; private set; }
+    public PlayerController InputController { get; private set; }
+    public CharacterController CharController { get; private set; }
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        AnimationData.Initialize();
+        Animator = GetComponent<Animator>();
+        InputController = GetComponent<PlayerController>();
+        CharController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ RPGProjectSource/Assets/2. Scripts/Player/StateMachine/StateMachine.cs
@@ -0,0 +1,41 @@
+﻿
+// 플레이어 상태들이 받을 상태 인터페이스
+public interface IPlayerState
+{
+    public void Enter();
+    public void Exit();
+    public void HandleInput();
+    public void Update();
+    public void PhysicsUpdate();
+}
+
+public abstract class StateMachine
+{
+    // 현재 상태
+    protected IPlayerState currentState;
+
+    // 상태 변경
+    public void ChangeState(IPlayerState state)
+    {
+        // 같은 상태라면 실행 X
+        if (currentState == state) return;
+
+        currentState?.Exit();
+        currentState = state;
+        currentState?.Enter();
+    }
+
+    public void HandleInput()
+    {
+        currentState?.HandleInput();
+    }
+
+    public void Update()
+    {
+        currentState?.Update();
+    }
+    public void PhysicsUpdate()
+    {
+        currentState?.PhysicsUpdate();
+    }
+}
