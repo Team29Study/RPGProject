e6a4b6e1d2ed422286307d0172ff9de80c8bd678
leewootak <leewt32414@gmail.com>
2025-03-27 20:49:46 +0900

    [feat] UIManager에 BaseUI, ShopUI 연결

--- RPGProjectSource/Assets/2. Scripts/UI/BaseUI.cs
+++ RPGProjectSource/Assets/2. Scripts/UI/BaseUI.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseUI : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // UIManager에 BaseUI 연결
+        UIManager.Instance.SetBaseUI(this);
     }
 }
