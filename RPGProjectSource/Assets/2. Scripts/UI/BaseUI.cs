7fcb2b166922c019e2764da2fcb97a0cd0d66505
leewootak <leewt32414@gmail.com>
2025-03-28 20:28:53 +0900

    [feat] 기본 및 상점 UI 추가

--- RPGProjectSource/Assets/2. Scripts/UI/BaseUI.cs
+++ RPGProjectSource/Assets/2. Scripts/UI/BaseUI.cs
@@ -10,10 +10,38 @@ public class BaseUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI gold;
     [SerializeField] private TextMeshProUGUI level;
     [SerializeField] private TextMeshProUGUI expText;
+    [SerializeField] private Button shopBtn;
 
-    void Start()
+    private void Start()
     {
         // UIManager에 BaseUI 연결
         UIManager.Instance.SetBaseUI(this);
     }
+
+    private void SetHpBar()
+    {
+
+    }
+    
+    private void SetMpBar()
+    {
+
+    }
+
+    private void SetExpBar()
+    {
+
+    }
+
+    private void SetGold()
+    {
+
+    }
+
+    private void SetLevel()
+    {
+
+    }
+
+
 }
