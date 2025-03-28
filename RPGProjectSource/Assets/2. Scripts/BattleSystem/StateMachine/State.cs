2d190314e653c225195cb02a622a99b1717128aa
hqahaad <grabq04@gmail.com>
2025-03-28 20:51:27 +0900

    [chore] 스크립트 충돌 정리

--- RPGProjectSource/Assets/2. Scripts/BattleSystem/StateMachine/State.cs
+++ RPGProjectSource/Assets/2. Scripts/BattleSystem/StateMachine/State.cs
@@ -1,149 +1,152 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public abstract class IState
+namespace Dev.Temp
 {
-    protected StateMachine stateMachine;
-
-    public IState(StateMachine newStateMachine)
+    public abstract class IState
     {
-        stateMachine = newStateMachine;
-        // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-    }
-    
-    public abstract void Enter();
-    public abstract void Update();
-    public abstract void Exit();
-}
+        protected StateMachine stateMachine;
 
-public class IdleState : IState
-{
-    public IdleState(StateMachine newStateMachine) : base(newStateMachine) { }
-
-    public override void Enter()
-    {
-        Debug.Log("Entered Idle State");
-    }
-
-    public override void Update()
-    {
-        Debug.Log("Updated Idle State");
-    }
-    public override void Exit() {}
-}
-
-public class TracingState : IState
-{
-    private static readonly int Run = Animator.StringToHash("Run");
-    private static readonly int Attack = Animator.StringToHash("Attack");
-
-    // 어디서 관리?
-    private Animator animator;
-    
-    public TracingState(StateMachine newStateMachine) : base(newStateMachine)
-    {
-        animator = stateMachine.GetComponent<Animator>();
-    }
-
-    public override void Enter()
-    {
-        Debug.Log("Entered the tracing state");
-        animator.SetBool(Run, true);
-        animator.SetBool(Attack, false);
-
-    }
-
-    public override void Update()
-    {
-        if (Mathf.Approximately(stateMachine.target.rotation.y, stateMachine.transform.rotation.y))
+        public IState(StateMachine newStateMachine)
         {
-            Debug.Log("show back position"); // 뒤인지 체크 (안됨)
-        }
-        
-        stateMachine.agent.SetDestination(stateMachine.target.position);
-    }
-    public override void Exit() {}
-}
-
-public class AttackState : IState
-{
-    private Animator animator;
-    private static readonly int Attack = Animator.StringToHash("Attack");
-
-    private NavMeshAgent navMeshAgent;
-    private HitBox hitbox;
-
-    public AttackStrategy attackStrategy;
-    
-    public AttackState(StateMachine newStateMachine) : base(newStateMachine)
-    {
-        animator = stateMachine.GetComponent<Animator>();
-        hitbox = stateMachine.GetComponentInChildren<HitBox>(true);
-        navMeshAgent = stateMachine.GetComponent<NavMeshAgent>();
-
-        attackStrategy = stateMachine.GetComponent<AttackStrategy>();
-    }
-
-    public override void Enter()
-    {
-        navMeshAgent.velocity = Vector3.zero;
-        // navMeshAgent.isStopped = true;
-        
-
-        attackStrategy.Attack();
-        // hitbox.gameObject.SetActive(true);
-        
-        animator.SetBool(Attack, true);
-    }
-
-    public override void Update()
-    {
-    }
-    public override void Exit() {}
-}
-
-public class HitState : IState
-{
-    private float knockBackDuration = 1f;
-    private float curerntKnockBackDuration = 0;
-    
-    public HitState(StateMachine newStateMachine) : base(newStateMachine)
-    {
-    }
-
-    public override void Enter()
-    {
-        stateMachine.agent.velocity = Vector3.zero;
-    }
-
-    public override void Update()
-    {
-        curerntKnockBackDuration += Time.deltaTime;
-        if (curerntKnockBackDuration >= knockBackDuration)
-        {
-            return;
+            stateMachine = newStateMachine;
+            // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
         }
 
-        stateMachine.transform.position += -stateMachine.transform.forward * 0.1f;
+        public abstract void Enter();
+        public abstract void Update();
+        public abstract void Exit();
     }
 
-    public override void Exit()
+    public class IdleState : IState
     {
-    }
-}
+        public IdleState(StateMachine newStateMachine) : base(newStateMachine) { }
 
-public class RushState : IState
-{
-    public RushState(StateMachine newStateMachine) : base(newStateMachine)
-    {
+        public override void Enter()
+        {
+            Debug.Log("Entered Idle State");
+        }
+
+        public override void Update()
+        {
+            Debug.Log("Updated Idle State");
+        }
+        public override void Exit() { }
     }
 
-    public override void Enter() {}
-    public override void Update()
+    public class TracingState : IState
     {
+        private static readonly int Run = Animator.StringToHash("Run");
+        private static readonly int Attack = Animator.StringToHash("Attack");
+
+        // 어디서 관리?
+        private Animator animator;
+
+        public TracingState(StateMachine newStateMachine) : base(newStateMachine)
+        {
+            animator = stateMachine.GetComponent<Animator>();
+        }
+
+        public override void Enter()
+        {
+            Debug.Log("Entered the tracing state");
+            animator.SetBool(Run, true);
+            animator.SetBool(Attack, false);
+
+        }
+
+        public override void Update()
+        {
+            if (Mathf.Approximately(stateMachine.target.rotation.y, stateMachine.transform.rotation.y))
+            {
+                Debug.Log("show back position"); // 뒤인지 체크 (안됨)
+            }
+
+            stateMachine.agent.SetDestination(stateMachine.target.position);
+        }
+        public override void Exit() { }
     }
 
-    public override void Exit()
+    public class AttackState : IState
     {
+        private Animator animator;
+        private static readonly int Attack = Animator.StringToHash("Attack");
+
+        private NavMeshAgent navMeshAgent;
+        private HitBox hitbox;
+
+        public AttackStrategy attackStrategy;
+
+        public AttackState(StateMachine newStateMachine) : base(newStateMachine)
+        {
+            animator = stateMachine.GetComponent<Animator>();
+            hitbox = stateMachine.GetComponentInChildren<HitBox>(true);
+            navMeshAgent = stateMachine.GetComponent<NavMeshAgent>();
+
+            attackStrategy = stateMachine.GetComponent<AttackStrategy>();
+        }
+
+        public override void Enter()
+        {
+            navMeshAgent.velocity = Vector3.zero;
+            // navMeshAgent.isStopped = true;
+
+
+            attackStrategy.Attack();
+            // hitbox.gameObject.SetActive(true);
+
+            animator.SetBool(Attack, true);
+        }
+
+        public override void Update()
+        {
+        }
+        public override void Exit() { }
+    }
+
+    public class HitState : IState
+    {
+        private float knockBackDuration = 1f;
+        private float curerntKnockBackDuration = 0;
+
+        public HitState(StateMachine newStateMachine) : base(newStateMachine)
+        {
+        }
+
+        public override void Enter()
+        {
+            stateMachine.agent.velocity = Vector3.zero;
+        }
+
+        public override void Update()
+        {
+            curerntKnockBackDuration += Time.deltaTime;
+            if (curerntKnockBackDuration >= knockBackDuration)
+            {
+                return;
+            }
+
+            stateMachine.transform.position += -stateMachine.transform.forward * 0.1f;
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+
+    public class RushState : IState
+    {
+        public RushState(StateMachine newStateMachine) : base(newStateMachine)
+        {
+        }
+
+        public override void Enter() { }
+        public override void Update()
+        {
+        }
+
+        public override void Exit()
+        {
+        }
     }
 }
--- RPGProjectSource/Assets/2. Scripts/BattleSystem/StateMachine/StateMachine.cs
+++ RPGProjectSource/Assets/2. Scripts/BattleSystem/StateMachine/StateMachine.cs
@@ -1,65 +1,68 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class StateMachine : MonoBehaviour
+namespace Dev.Temp
 {
-    public int detectDistance;
-    
-    private IState previousState;
-    private IState currentState;
-    
-    private IState idleState;
-    private IState tracingState;
-    private IState attackState;
-    
-    [HideInInspector] public NavMeshAgent agent; // 어떤 위치에 있어야할까?
-    public Transform target;
-    
-
-    public void ChangeState(IState newState)
+    public class StateMachine : MonoBehaviour
     {
-        currentState?.Exit();
-        currentState = newState;
-        newState.Enter();
-    }
+        public int detectDistance;
 
-    private void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
+        private IState previousState;
+        private IState currentState;
 
-        // 프리팹 정보 외 씬의 플레이어 인스턴스 등록할 방법이 없음
-        if (target == null)
+        private IState idleState;
+        private IState tracingState;
+        private IState attackState;
+
+        [HideInInspector] public NavMeshAgent agent; // 어떤 위치에 있어야할까?
+        public Transform target;
+
+
+        public void ChangeState(IState newState)
         {
-            target = GameObject.FindGameObjectWithTag("Player").transform;
-        }
-    }
-
-    private void Start()
-    {
-        // 확장될 때마다 생김
-        idleState = new IdleState(this);
-        tracingState = new TracingState(this);
-        attackState = new AttackState(this);
-        
-        ChangeState(idleState);
-    }
-
-    // 여기서 관리하면? 몬스터마다 상태가 달라질 수 있음(의존성이 떨어짐)
-    // 상태에서 관리하면? ...
-    private void Update()
-    {
-        if(currentState != null) {currentState.Update();}
-        
-        if (currentState != tracingState && Vector3.Distance(transform.position, target.position) <= detectDistance && Vector3.Distance(transform.position, target.position) > agent.stoppingDistance)
-        {
-            ChangeState(tracingState);
-            return;
+            currentState?.Exit();
+            currentState = newState;
+            newState.Enter();
         }
 
-        if (currentState != attackState && Vector3.Distance(transform.position, target.position) <= agent.stoppingDistance)
+        private void Awake()
         {
-            ChangeState(attackState);
-            return;
+            agent = GetComponent<NavMeshAgent>();
+
+            // 프리팹 정보 외 씬의 플레이어 인스턴스 등록할 방법이 없음
+            if (target == null)
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+        }
+
+        private void Start()
+        {
+            // 확장될 때마다 생김
+            idleState = new IdleState(this);
+            tracingState = new TracingState(this);
+            attackState = new AttackState(this);
+
+            ChangeState(idleState);
+        }
+
+        // 여기서 관리하면? 몬스터마다 상태가 달라질 수 있음(의존성이 떨어짐)
+        // 상태에서 관리하면? ...
+        private void Update()
+        {
+            if (currentState != null) { currentState.Update(); }
+
+            if (currentState != tracingState && Vector3.Distance(transform.position, target.position) <= detectDistance && Vector3.Distance(transform.position, target.position) > agent.stoppingDistance)
+            {
+                ChangeState(tracingState);
+                return;
+            }
+
+            if (currentState != attackState && Vector3.Distance(transform.position, target.position) <= agent.stoppingDistance)
+            {
+                ChangeState(attackState);
+                return;
+            }
         }
     }
 }
