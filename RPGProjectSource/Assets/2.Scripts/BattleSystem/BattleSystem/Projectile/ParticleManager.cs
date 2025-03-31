cdbf49f70398fcb65d86ed7a4fe6f7079e93c879
Junebeom Lee <junebeom.dev@gmail.com>
2025-03-31 22:00:24 +0900

    [feat]동적으로 맵 생성

--- RPGProjectSource/Assets/2.Scripts/BattleSystem/BattleSystem/Projectile/ParticleManager.cs
+++ RPGProjectSource/Assets/2.Scripts/BattleSystem/BattleSystem/Projectile/ParticleManager.cs
@@ -1,6 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ParticleManager: Singleton<ParticleManager>
 {
-    public static ParticleManager Instance { get; private set; }
+    public List<GameObject> particleList;
+    private List<(int index, ParticleSystem particle)> pool = new();
+
+    public void CreateParticle(Transform target, int index)
+    {
+        // var selectedProjectile = pool.Find(projectile => projectile.index == index && !projectile.gameObject.gameObject.activeSelf);
+        //
+        // if (selectedProjectile)
+        // {
+        //     selectedProjectile.gameObject.transform.SetPositionAndRotation(target.position, target.rotation);
+        //     selectedProjectile.gameObject.gameObject.SetActive(true);
+        //     return;
+        // }
+        //
+        // var projectile = Instantiate(projectileList[Random.Range(0, projectileList.Count)], target.position + Vector3.up, target.rotation, transform);
+        // pool.Add((index, projectile));
+    }
+    
+    // public void DestroyProjectile(GameObject particle)
+    // { 
+    //     particle.SetActive(false);
+    //     particle.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+    // }
 }
