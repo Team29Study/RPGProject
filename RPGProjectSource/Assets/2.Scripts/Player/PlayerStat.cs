0aabbf042ad94dcc273e43052797884ce8a302c2
park8306 <kosaf0804@gmail.com>
2025-04-01 13:02:46 +0900

    [refactor] PlayerStat이 맡고 있던 역할 PlayerHealth와 PlayrAttack으로 분리

--- RPGProjectSource/Assets/2.Scripts/Player/PlayerStat.cs
+++ RPGProjectSource/Assets/2.Scripts/Player/PlayerStat.cs
@@ -15,7 +15,7 @@ public enum PlayerStatType
     DefencePower   // 방어력
 }
 
-public class PlayerStat : MonoBehaviour , IDamagable
+public class PlayerStat : MonoBehaviour
 {
     public Player player;
 
@@ -70,87 +70,5 @@ public class PlayerStat : MonoBehaviour , IDamagable
     private void Update()
     {
         mediator.Update(Time.deltaTime);
-    }
-
-    public void TakeDamage(int damage, Transform attackTr = null)
-    {
-        Vector3 attackDir = (new Vector3(attackTr.position.x, 0, attackTr.position.z) - new Vector3(transform.position.x, 0, transform.position.z)).normalized;
-
-        // 막기 불가능하다면 데미지를 입힘
-        if (!PossibleBlock(attackDir))
-            HP = Mathf.Max(HP - CalculateDef(damage), 0);
-
-        if(HP == 0)
-        {
-            // 죽음
-            Debug.Log("플레이어 사망");
-        }
-    }
-
-    private int CalculateDef(int damage)
-    {
-        return damage - DefencePower;
-    }
-
-    // 가드가 가능한지 확인
-    public bool PossibleBlock(Vector3 attackDir)
-    {
-        if (player.stateMachine.IsBlocking)
-        {
-            float dot = Vector3.Dot(transform.forward.normalized, attackDir.normalized);
-            float degree = Mathf.Acos(dot) * Mathf.Rad2Deg;
-
-            Debug.Log(degree);
-            if (degree > 30)
-                return false;
-            else
-                return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-
-    public void OnAttackMonster()
-    {
-        // 히트박스 생성
-        ProjectileManager.Instance.CreateMeleeAttack(transform, true);
-        HitBox hitBox = GetComponentInChildren<HitBox>();
-
-        if(hitBox)
-        {
-            // 이벤트 등록
-            hitBox.onTrigger += Attack;
-        }
-    }
-
-    public void OffAttackMonster()
-    {
-        ProjectileManager.Instance.CreateMeleeAttack(transform, false);
-
-        HitBox hitBox = GetComponentInChildren<HitBox>();
-
-        if(hitBox)
-        {
-            // 이벤트 해제
-            hitBox.onTrigger -= Attack;
-        }
-    }
-
-    public void Attack(Collider collider)
-    {
-        // 공격의 기본 데미지
-        int attackDamage = player.Data.AttackData.AttackInfoDatas[player.stateMachine.ComboIndex].Damage;
-        // 캐릭터의 공격력 추가
-        attackDamage += AttackPower;
-
-        IDamagable enemy = collider.GetComponent<IDamagable>();
-
-        if (enemy != null)
-        {
-            enemy.TakeDamage(attackDamage);
-        }
     }
 }
