1ad9c498c50cd23e826c8d20a6c41ea444e5ea81
leewootak <leewt32414@gmail.com>
2025-04-02 15:41:35 +0900

    [feat] 상점 화면에 판매 슬롯 및 아이템 목록 추가

--- RPGProjectSource/Assets/2.Scripts/UI/ShopUI.cs
+++ RPGProjectSource/Assets/2.Scripts/UI/ShopUI.cs
@@ -10,13 +10,22 @@ public class ShopUI : MonoBehaviour
     [SerializeField] private Button sellBtn;
     [SerializeField] private TextMeshProUGUI description;
 
+    [Header("Buy")]
     [SerializeField] private Transform slot;
     [SerializeField] private ShopSlot slotPrefab;
+
+    [Header("Sell")]
+    [SerializeField] private Transform sSlot;
+    [SerializeField] private SellSlot sellSlotPrefab;
 
     public List<ShopSlot> shopSlots = new(); // 상점 UI에 표시될 아이템 슬롯 리스트
     public List<ItemData> shopItemDatas = new(); // 상점에 판매되는 아이템 데이터 리스트
 
+    public List<SellSlot> sellSlots = new(); // 판매 목록 아이템 슬롯 리스트
+    public List<ItemData> invenItemDatas = new(); // 인벤토리 아이템 리스트 (임시)
+
     private ShopSlot shopSlot;
+    private SellSlot sellSlot;
 
     private void Start()
     {
@@ -28,6 +37,8 @@ public class ShopUI : MonoBehaviour
         sellBtn.onClick.AddListener(Sell); // 아이템 판매 버튼에 리스너 추가
 
         InitShopSlot(); // 상점 슬롯 초기화
+        InitSellSlot();
+        description.text = null;
     }
 
     private void InitShopSlot()
@@ -53,21 +64,57 @@ public class ShopUI : MonoBehaviour
         }
     }
 
-    public void UpdateShopSlot()
+    private void InitSellSlot()
     {
-        // 슬롯들을 갱신
-        for (int i = 0; shopSlots.Count > i; i++)
+        // 기존 슬롯들 삭제
+        foreach (Transform child in sSlot)
         {
+            Destroy(child.gameObject);
+        }
+        sellSlots.Clear(); // 슬롯 리스트 초기화
+
+        // 새로운 슬롯들을 생성하여 상점에 추가
+        for (int i = 0; i < invenItemDatas.Count; i++)
+        {
+            SellSlot newSlot = GameObject.Instantiate(sellSlotPrefab, sSlot);
+
             if (i < shopItemDatas.Count)
             {
-                shopSlots[i].SetShopItem(shopItemDatas[i]); // 아이템 데이터 갱신
+                newSlot.SetSellItem(invenItemDatas[i]); // 아이템 데이터 설정
             }
-            else
-            {
-                shopSlots.Add(null); // 빈 슬롯 추가
-            }
+
+            sellSlots.Add(newSlot); // 슬롯 리스트에 추가
         }
     }
+
+    //public void UpdateShopSlot()
+    //{
+    //    // 구매목록 슬롯 갱신
+    //    for (int i = 0; shopSlots.Count > i; i++)
+    //    {
+    //        if (i < shopItemDatas.Count)
+    //        {
+    //            shopSlots[i].SetShopItem(shopItemDatas[i]); // 아이템 데이터 갱신
+    //        }
+    //        else
+    //        {
+    //            shopSlots.Add(null); // 빈 슬롯 추가
+    //        }
+    //    }
+
+    //    // 판매목록 슬롯 갱신
+    //    for (int i = 0; sellSlots.Count > i; i++)
+    //    {
+    //        if (i < invenItemDatas.Count)
+    //        {
+    //            sellSlots[i].SetSellItem(invenItemDatas[i]); // 아이템 데이터 갱신
+    //        }
+    //        else
+    //        {
+    //            sellSlots.Add(null); // 빈 슬롯 추가
+    //        }
+    //    }
+    //}
 
     public void ShowDescription(ItemData itemData)
     {
@@ -78,6 +125,11 @@ public class ShopUI : MonoBehaviour
     public void SelectSlot(ShopSlot slot)
     {
         shopSlot = slot; // 슬롯 선택
+    }
+
+    public void SelectSellSlot(SellSlot slot)
+    {
+        sellSlot = slot;
     }
 
     private void Buy()
